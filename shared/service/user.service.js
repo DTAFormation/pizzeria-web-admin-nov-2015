dfb7b8a6397bd751c8eaa80fcadd9429b77c9048
Olivier <sta.olivier@gmail.com>
2015-11-13 11:06:56 +0100

    Rereboot

---
+++ shared/service/user.service.js
@@ -0,0 +1,17 @@
+angular.module('pzWebAdminApp.shared').service('userService', function($http, $q) {
+
+    var connected = false;
+
+    this.isConnected = function() {
+        return connected;
+    };
+
+    this.login = function(login, password) {
+        // TODO : Gestion de la connexion
+    };
+
+    this.logout = function() {
+        // TODO Gestion de la déconnexion
+    };
+
+});
--- src/app.config.js
+++
@@ -1,8 +0,0 @@
-import angular from 'angular';
-
-export default function routing($urlRouterProvider, $locationProvider) {
-  $locationProvider.html5Mode(true);
-
-  $urlRouterProvider
-  .otherwise('/order');
-}
