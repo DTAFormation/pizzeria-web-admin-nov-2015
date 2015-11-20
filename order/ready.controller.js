211baadc59a100e842f239bfa2d1769fee7dc12f
Vincent LANDRA <gvince958@gmail.com>
2015-11-20 12:58:17 +0100

    #4 #7 Ajout et fin detail Commande

--- order/ready.controller.js
+++ order/ready.controller.js
@@ -1,11 +1,14 @@
 angular.module('pzWebAdminApp.order').controller('ReadyController', function($state, PizzaService, DrinkService, CommandService){
   var ctrl = this;
+  ctrl.toggle = {};
   ctrl.pizzaPretes = {};
 
   ctrl.listUpdate = function (){
   	CommandService.getCommandesPretesCaisse()
     .then(function (commandes) {
         ctrl.pizzaPretes = commandes;
+        for (var i = 0; i < ctrl.pizzaPretes.length; i++)
+          ctrl.toggle[i] = false;
     }.bind(ctrl));
   };
 
